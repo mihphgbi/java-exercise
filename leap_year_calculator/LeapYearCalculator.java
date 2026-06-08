79407f7796760671dc9f96c08c539add72deb5e1
Minh Phuong <83202708+mihphgbi@users.noreply.github.com>
2026-06-09 00:01:42 +0700

    LJ-06: Leap year calculator

---
+++ leap_year_calculator/LeapYearCalculator.java
@@ -0,0 +1,17 @@
+package leap_year_calculator;
+
+import java.util.Scanner;
+
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year;
+        System.out.print("Enter the year: ");
+        year = input.nextInt();
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println("The year is a leap year");
+        }else {
+            System.out.println("The year is not a leap year");
+        }
+    }
+}
