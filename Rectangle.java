9d92dd642c149f809c4b6775d4f8558d5ac613e3
Minh Phuong <83202708+mihphgbi@users.noreply.github.com>
2026-06-08 23:35:31 +0700

    LJ-03: Operator - Expression

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    static void main() {
+     float width;
+     float height;
+     Scanner scanner = new Scanner(System.in);
+
+     System.out.println("Enter width");
+     width = scanner.nextFloat();
+     System.out.println("Enter height");
+     height = scanner.nextFloat();
+     System.out.println("Area of rectangle is: " + (width * height));
+    }
+}
