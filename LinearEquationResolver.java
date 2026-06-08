b26af27a085930bd72ac1eb79ed6bf985e076645
Minh Phuong <83202708+mihphgbi@users.noreply.github.com>
2026-06-08 23:44:47 +0700

    LJ-04: Linear Equation Resolver

---
+++ LinearEquationResolver.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Linear Equation Resolver");
+        System.out.println("Given a linear equation as ax + b = c, please enter constants:");
+        System.out.println("a: ");
+        double a = scanner.nextDouble();
+        System.out.println("b: ");
+        double b = scanner.nextDouble();
+        System.out.println("c: ");
+        double c = scanner.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) / a;
+            System.out.printf("Equation pass with x = %f!\n: ", answer);
+        } else {
+            if (b == c) {
+                System.out.println("The solution is all x!");
+            } else {
+                System.out.println("No solution!");
+            }
+        }
+    }
+}
