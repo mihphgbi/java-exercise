f75d64ba5bb70cb74e53b82037532ba98e4f0176
Minh Phuong <83202708+mihphgbi@users.noreply.github.com>
2026-06-15 21:07:11 +0700

    LJ-18: Prime v3

---
+++ ex2/prime/PrimeV3.java
@@ -0,0 +1,20 @@
+package ex2.prime;
+
+public class PrimeV3 {
+    public static void main(String[] args) {
+        for (int number = 2; number <= 100; number++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.println(number);
+            }
+        }
+    }
+}
